5c3d305106a5b03ffb1c8413c464ccc6433a39b4
Ankit <Ankit@DESKTOP-Q6D4D6J>
2017-02-25 13:23:28 +0530

    Add project files.

---
+++ AngularJS/JavaScript/MyScript.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="angular.min.js" />
+
+var myApp = angular.module("myModule", []);
+
+
+myApp.controller("mycontroller", function ($scope)
+{
+    $scope.message = "First Time";
+})
